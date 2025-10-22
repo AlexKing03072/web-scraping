dcd9f69837822ae8d22bc6a7d4d2c809032301ef
LiXiang <abcdefg32155@gmail.com>
2025-10-23 00:01:10 +0800

    第一次上傳專案

---
+++ scrape1.py
@@ -0,0 +1,13 @@
+import requests, re
+
+
+url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
+response = requests.get(url)
+
+# Find all matches of the price pattern(找出所有符合價格模式的項目)
+#使用raw string避免跳脫字元的影響
+
+match = re.findall(r'£\d+\.\d{2}', response.text)
+
+# Print the matched prices(印出所有符合的價格)
+print(match)
