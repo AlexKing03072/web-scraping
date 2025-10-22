21193f9a0386afa13de2fa6db5c95cf6ad72c207
LiXiang <abcdefg32155@gmail.com>
2025-10-23 00:26:41 +0800

    縮排

--- scrape1.py
+++ scrape1.py
@@ -5,7 +5,7 @@ url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
 response = requests.get(url)
 
 # Find all matches of the price pattern(找出所有符合價格模式的項目)
-#使用raw string避免跳脫字元的影響
+# 使用raw string避免跳脫字元的影響
 
 match = re.findall(r'£\d+\.\d{2}', response.text)
 
